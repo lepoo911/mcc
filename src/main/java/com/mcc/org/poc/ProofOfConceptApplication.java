90ee723c84ab19cfc2f7735749fdaf4c668079e7
stefan buchholz <tubywuby@gmail.com>
2020-06-23 15:40:51 -0400

    boilerplate

---
+++ src/main/java/com/mcc/org/poc/ProofOfConceptApplication.java
@@ -0,0 +1,13 @@
+package com.mcc.org.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofOfConceptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProofOfConceptApplication.class, args);
+    }
+
+}
